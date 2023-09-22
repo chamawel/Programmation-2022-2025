35214592bb33af5b8e244f0735fcae72f62cf209
chamawel <ledentmael@gmail.com>
2023-09-22 09:34:07 +0200

    Added more comments for the teacher

--- ex-1p4.py
+++ ex-1p4.py
@@ -1,8 +1,12 @@
+#init
+
 gsm         = float(input("Entrer prix Gsm")) #Input
 
 protections = float(input("Entrer prix protection")) #Input
 
 screens     = float(input("Entrer prix écran")) #Input
+
+#Processing and Output
 
 print(f"Le montant à payer est de: {(gsm+protections+screens)}") #Output
 
