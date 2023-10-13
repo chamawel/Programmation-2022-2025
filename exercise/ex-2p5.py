9c13e82c6e40f7a81bf45bd6fffec5291506dce7
chamawel <ledentmael@gmail.com>
2023-10-13 09:49:18 +0200

    added directories to organise

--- exercise/ex-2p5.py
+++
@@ -1,9 +0,0 @@
-#Init
-
-length = float(input("Entrez Une Longueur (n.nn) ")) #Input
-
-height = float(input("Entrez Une Hauteur (n.nn)")) #Input
-
-#Processing and output
-
-print(f"la surface est de {length*height}m² ") # Output
---
+++ exercise/les-base/ex-3p5.py
@@ -0,0 +1,21 @@
+#first Init
+
+a = 3   #Afectation  
+b = 5   #Afectation  
+
+# output
+
+print(f"'a' vaut {a} et 'b' vaut {b} ") #output
+
+#Processing
+
+a = a + b   #Afectation  
+b = a - b   #Afectation  
+a = a - b   #Afectation  
+
+#Output
+
+print(f"'a' vaut {a} et 'b' vaut {b} ") #Output
+
+
+# Le XOR ('^') ne fonctionne pas :(
