9c99d4cef926675668d86c04a117fa9e67c2d90b
chamawel <ledentmael@gmail.com>
2023-10-13 09:42:49 +0200

    changed a ex for the teacher

--- exercise/ex-3p9.py
+++ exercise/ex-3p9.py
@@ -1,25 +1,44 @@
 import time
 
 while True:
-    u_input      = input("Donnez les âge (séparé par des espace: a b c)")
-    u_input_list = u_input.split()
-    print('liste',u_input_list)
 
-    for i in range(len(u_input_list)):
-        u_input_list[i] = int(u_input_list[i])
+    # | AVEC LISTE  (plus stylé) |
+
+    # u_input      = input("Donnez les âge (séparé par des espace: a b c)")
+    # u_input_list = u_input.split()
+    # print('liste',u_input_list)
+
+    # for i in range(len(u_input_list)):
+    #     u_input_list[i] = int(u_input_list[i])
+
+    # print("Liste Convertie",u_input_list)
 
 
-    result = u_input_list.index(min(u_input_list))
+    # result = u_input_list.index(min(u_input_list))
 
-    if result == 0:
-        print("Le premier joueur a priorité")
-    elif result == 1:
-        print("Le second joueur a priorité")
-    else:
-        print("Le dernier joueur a priorité")
+    # print(f"Le joueur avec la priorité est le joueur {result+1} " )
 
-    isquitting = input("Voulez-vos quitter ? (Y/N)")
+    # isquitting = input("Voulez-vos quitter ? (Y/N)")
 
-    if isquitting == "Y" or isquitting == "y":
-        exit("exiting Program ...")
+    # if isquitting == "Y" or isquitting == "y":
+    #     exit("exiting Program ...")
+
+    # | SANS LISTE (vachement moins cool) |
     
+    a = int(input("age 1er joueur"))
+    b = int(input("age 2eme joueur"))
+    c = int(input("age 3eme joueur"))
+
+    if   a < b and a <c:
+        print("Le joueur 1 est prioritaire")
+        
+    elif b < a and b <c:
+        print("Le joueur 2 est prioritaire")
+        
+    elif c <a and c<b:
+        print("Le joueur 3 est prioritaire")
+        
+    else:
+        print("Il y a une égalité")
+    
+    time.sleep(1)
