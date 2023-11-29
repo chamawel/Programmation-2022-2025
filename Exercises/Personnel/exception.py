36b647ea94ff61f06a7867922d164d431161ce1f
chamawel <ledentmael@gmail.com>
2023-11-29 11:45:01 +0100

    tried 'execpt' and 'try' for fun

---
+++ Exercises/Personnel/exception.py
@@ -0,0 +1,13 @@
+from tkinter.font import BOLD
+from colorama import Back, Fore, Style
+
+while True:
+    try:
+        num = int(input("Donnez un nombre"))
+        print(f"Votre nombre + 1 : {Back.GREEN} {num+1} {Back.RESET} ")
+        break
+    except ValueError:
+        print(Fore.RED + "Vous devez entré un nombre")
+        print(Fore.RESET)
+    finally:
+        print(f" {Style.BRIGHT} BOO {Style.RESET_ALL}")
