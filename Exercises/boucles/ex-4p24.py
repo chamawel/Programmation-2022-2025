2d456ad94ee69f9b5ed72b04382228cb5a8ea350
chamawel <ledentmael@gmail.com>
2024-01-29 08:52:19 +0100

    quizz - fixing the loop

--- Exercises/boucles/ex-4p24.py
+++ Exercises/boucles/ex-4p24.py
@@ -1,17 +1,19 @@
 from random import randint
-question = 20
+question = 5
 lifes = 2
 score = 0
 c = 0
 
-while c <= question:
+while c < question:
+    c += 1
     randint_1 = randint(2,10)
     randint_2 = randint(2,10)
     user_respond = int(input(f"Que vaut {randint_1} x {randint_2} "))
     
     if user_respond == randint_1*randint_2:
-        print("Bravo")
         score +=1
+        print("Bravo")
+        
     else:
         print("raté")
         lifes -=1
@@ -20,3 +22,4 @@ while c <= question:
             print(f"raté , la bonne réponse était {randint_1*randint_2} ")
 
 print(f"votre score : {score} ")
+
