6ce229126104fedba2b2f1109dde6bdea6693ce2
chamawel <ledentmael@gmail.com>
2024-02-05 08:49:28 +0100

    changed the 'randint' into 'randrange' cus object is a big nono for the teacher

--- Exercises/boucles/ex-4p24.py
+++ Exercises/boucles/ex-4p24.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randrange
 question = 20
 lifes = 2
 score = 0
@@ -6,16 +6,16 @@ c = 0
 
 while c < question:
     c += 1
-    randint_1 = randint(2,10)
-    randint_2 = randint(2,10)
-    user_respond = int(input(f"Que vaut {randint_1} x {randint_2} "))
+    randrange_1 = randrange(2,11)
+    randrange_2 = randrange(2,11)
+    user_respond = int(input(f"Que vaut {randrange_1} x {randrange_2} "))
     
-    if user_respond == randint_1*randint_2:
+    if user_respond == randrange_1*randrange_2:
         score +=1
         print("Bravo")
         
     else:
-        print(f"raté, la bonne réponse était {randint_1*randint_2}  ")
+        print(f"raté, la bonne réponse était {randrange_1*randrange_2}  ")
         lifes -=1
 
         if lifes == 0:
