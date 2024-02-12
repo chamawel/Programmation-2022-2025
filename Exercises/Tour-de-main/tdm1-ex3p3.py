6e44ea8abf0d18262637ba676d3f77d37527cb20
chamawel <ledentmael@gmail.com>
2024-02-12 09:57:37 +0100

    added third ex

---
+++ Exercises/Tour-de-main/tdm1-ex3p3.py
@@ -0,0 +1,14 @@
+num_of_reds   = 0
+num_of_blues_ = 0
+seen = ""
+
+while seen != "S" or "s" :
+
+    if seen == "R":
+        num_of_reds += 1
+
+    elif seen == "B":
+        num_of_blues_ += 1
+
+    
+
