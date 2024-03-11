bea4166219647bb0ebbda64000660a8ccdcf7e59
chamawel <ledentmael@gmail.com>
2024-03-11 09:11:38 +0100

    fixed the power of zero

--- Exercises/Tour-de-main/tdm1-ex2p2.py
+++ Exercises/Tour-de-main/tdm1-ex2p2.py
@@ -3,9 +3,13 @@ exponant = int(input("Donnez l'exposant"))
 i_number = number
 c = 1
 
-while c < exponant:
-    c += 1
-    number = number * i_number
+if exponant == 0:
+    number = 1
+
+else:    
+    while c < exponant:
+        c += 1
+        number = number * i_number
     
 
 print(f" {i_number} exposant {exponant} vaut : {number} ") 
