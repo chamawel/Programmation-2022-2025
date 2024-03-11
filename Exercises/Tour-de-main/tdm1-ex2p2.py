f892027fdcc44d9939856463f5fbc1c634e1ce7b
chamawel <ledentmael@gmail.com>
2024-03-11 09:20:41 +0100

    found a better way to do it

--- Exercises/Tour-de-main/tdm1-ex2p2.py
+++ Exercises/Tour-de-main/tdm1-ex2p2.py
@@ -1,15 +1,11 @@
-number = int(input("Donnez un nombre"))
+i_number = int(input("Donnez un nombre"))
 exponant = int(input("Donnez l'exposant"))
-i_number = number
-c = 1
+number = 1
+c = 0
 
-if exponant == 0:
-    number = 1
-
-else:    
-    while c < exponant:
-        c += 1
-        number = number * i_number
+while c < exponant:
+    number = number * i_number
+    c += 1
     
 
 print(f" {i_number} exposant {exponant} vaut : {number} ") 
