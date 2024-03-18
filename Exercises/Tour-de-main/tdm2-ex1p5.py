06d5bbe009799965bef605409e5ab68075454c2a
chamawel <ledentmael@gmail.com>
2024-03-18 08:44:32 +0100

    added a new exercise 'tdm2-ex1p5'

---
+++ Exercises/Tour-de-main/tdm2-ex1p5.py
@@ -0,0 +1,8 @@
+number = int(input("Donnez Un nombre"))
+sum = 0
+
+while number != 0:
+    sum += number
+    number = int(input("Donnez Un Nombre"))
+
+print(f"La Somme des nombres Entré vaut : {sum}")
