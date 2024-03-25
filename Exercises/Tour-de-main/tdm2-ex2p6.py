4021ad4f0b5f33a413abb7d8595c82ca0c761d31
chamawel <ledentmael@gmail.com>
2024-03-25 09:20:03 +0100

    changed 'tdm2-ex2p6' cus why not

--- Exercises/Tour-de-main/tdm2-ex2p6.py
+++ Exercises/Tour-de-main/tdm2-ex2p6.py
@@ -6,9 +6,11 @@ just_fix_windows_console()      # Pour le CMD de windows
 
 # / Variables / 
 
-index = 0
-picked_number = 0
-sum = 0
+index = 1
+picked_number = randrange(1,7)
+sum = picked_number
+print(f"Le Lancé n° {Back.YELLOW} {index} {Back.RESET} vaut {Back.GREEN} {picked_number} {Back.RESET} \n -> la somme totale de tout les lancé est de {Back.CYAN} {sum} {Back.RESET} \n ")
+
 
 # / Main Loop /
 
