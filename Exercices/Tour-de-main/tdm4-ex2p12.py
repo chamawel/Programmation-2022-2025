dc43f2d21a34d82982c79769a8a4be683169d430
chamawel <ledentmael@gmail.com>
2024-05-27 09:11:23 +0200

    oops seems like i can't read

--- Exercices/Tour-de-main/tdm4-ex2p12.py
+++ Exercices/Tour-de-main/tdm4-ex2p12.py
@@ -7,21 +7,27 @@ just_fix_windows_console()      # Pour le CMD de windows
 # / Variables / 
 
 index         : int = 1
+one_is_rolled : int = 0
+
 picked_number : int = randrange(1,7)
-sum           : int = picked_number
 last_num      : int = picked_number
 
-print(f"Le Lancé n° {Back.YELLOW} {index} {Back.RESET} vaut {Back.GREEN} {picked_number} {Back.RESET} \n -> la somme totale de tout les lancé est de {Back.CYAN} {sum} {Back.RESET} \n ")
+
+
+print(f"Le Lancé n° {Back.YELLOW} {index} {Back.RESET} vaut {Back.GREEN} {picked_number} {Back.RESET} \n")
 
 
 # / Main Loop /
 
 while last_num != 5 or picked_number != 6 :
-
+    
     last_num = picked_number
     index +=1
     picked_number = randrange(1,7)
-    sum += picked_number
+
+    if picked_number == 1:
+        one_is_rolled += 1
     
     
-    print(f"Le Lancé n° {Back.YELLOW} {index} {Back.RESET} vaut {Back.GREEN} {picked_number} {Back.RESET} \n -> la somme totale de tout les lancé est de {Back.CYAN} {sum} {Back.RESET} \n ")
+    
+    print(f"Le Lancé n° {Back.YELLOW} {index} {Back.RESET} vaut {Back.GREEN} {picked_number} {Back.RESET} le N° 1 a été roullé : {Back.CYAN} {one_is_rolled} fois {Back.RESET} \n")
