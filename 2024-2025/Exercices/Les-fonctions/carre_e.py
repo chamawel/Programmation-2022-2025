4936ccfa73984482a9001179e055f73daedc9fcf
Chamawel <ledentmael@gmail.com>
2025-01-16 15:08:54 +0100

    modified 'draw_square' function to include coordonates

--- 2024-2025/Exercices/Les-fonctions/carre_e.py
+++ 2024-2025/Exercices/Les-fonctions/carre_e.py
@@ -1,34 +1,37 @@
 from turtle import forward, left, color, goto, penup, pendown, done
 
-def draw_square(lenght: int, s_color : str) -> None:
-    for sides in range(4):
-        color(s_color)
-        forward(lenght)
-        left(90)
+def draw_square(lenght: int, s_color : str, x_cord : int, y_cord : int) -> None:
+        """Draw a square with the following caracteristics.
+            Lenght  : n° of px of the sides 
+            s_color : square color
+            x_cord  : x coordonate where the square starts
+            y_cord  : y coordonate where the square starts
+        """
+        
+        penup()
+        goto(x_cord,y_cord)
+        pendown()
+        
+        for sides in range(4):
+            color(s_color)
+            forward(lenght)
+            left(90)
 
 def draw_all_squares() -> None:
-    penup()
-    goto(30,10)
-    pendown()
-    draw_square(100, "green")
+    """Call 'draw_square' four times to create the windows logo.
+    """
 
-    penup()
-    goto(-90,10)
-    pendown()
-    draw_square(100, "red")
+    draw_square(100, "green", 30, 10)
 
-    penup()
-    goto(-90,-100)
-    pendown()
-    draw_square(100, "blue")
+    draw_square(100, "red", -90, 10)
 
-    penup()
-    goto(30,-100)
-    pendown()
-    draw_square(100, "yellow")
+    draw_square(100, "blue", -90, -100)
+
+    draw_square(100, "yellow", 30, -100)
 
 
 # MAIN
 
 draw_all_squares()
+done()
 
--- 2024-2025/Exercices/Les-fonctions/carre_f.py
+++ 2024-2025/Exercices/Les-fonctions/carre_f.py
@@ -1,21 +1,20 @@
 from turtle import forward, left, color, goto, penup, pendown, done
 
-def draw_square(lenght: int, s_color : str) -> None:
+def draw_square(lenght: int, s_color : str, x_cord : int, y_cord : int) -> None:
+    penup()
+    goto(x_cord,y_cord)
+    pendown()
+
     for sides in range(4):
         color(s_color)
         forward(lenght)
         left(90)
 
 def draw_all_squares(first_square : int,seccond_square : int) -> None:
-    penup()
-    goto(30,10)
-    pendown()
-    draw_square(first_square, "green")
 
-    penup()
-    goto(-90,10)
-    pendown()
-    draw_square(seccond_square, "red")
+    draw_square(first_square, "green", 30, 10)
+
+    draw_square(seccond_square, "red", -90, 10)
 
 
 
