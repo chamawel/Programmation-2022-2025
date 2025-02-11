b484fea908e17ab68412d130ad4ce0cdebf373b4
Chamawel <ledentmael@gmail.com>
2025-02-11 10:05:31 +0100

    added ex

--- 2024-2025/Exercices/Les-fonctions/ex-11p7.py
+++ 2024-2025/Exercices/Les-fonctions/ex-11p7.py
@@ -1,15 +1,17 @@
-def boxVolume(lenght : float = -1, height : float = -1, depth : float = -1) -> float:
-    if lenght != -1 and height != -1 and depth != -1:
+def boxVolume(lenght : float = -1, height : float = -1, depth : float = -1) -> float: 
+    if lenght == -1:
+        return -1
+    
+    elif height == -1:
+        return lenght**3
+    
+    elif depth == -1:
+        return lenght**2 * height
+    
+    else:
         return lenght * height * depth
 
-    elif lenght != -1 and height != -1:
-        return  lenght**2 * height
 
-    elif lenght != -1:
-       return lenght**3 
-
-    else:
-        return -1
 
 # Main
 print(boxVolume())
---
+++ 2024-2025/Exercices/Les-fonctions/ex-12p7.py
@@ -0,0 +1,21 @@
+def replaceChar(phrase : str, char1 : str, char2 : str, start : int = 0, end : int = -1) -> str:
+    """This Function replaces a char by another 
+    """
+
+    if end == -1:
+        end = len(phrase)
+
+    transformed_phrase : str = " "
+
+    for i in range(start,end):
+        if phrase[i] == char1:
+            transformed_phrase += char2
+        
+        else:
+            transformed_phrase += phrase[i]
+
+    return transformed_phrase
+ 
+phrase = "aeaeae"
+
+print(replaceChar(phrase,"e","b"))
