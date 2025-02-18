167db5447205b4d7d2edcd7a03bfaf14176f9dac
Chamawel <ledentmael@gmail.com>
2025-02-18 09:14:57 +0100

    udp readme , and added ex

---
+++ 2024-2025/Exercices/Sushi_dice/main.py
@@ -0,0 +1,13 @@
+from random import randrange
+
+
+def main() -> None:
+    player_set : list = [0] * 6
+    
+    input("Appuyer sur une touche pour lancé les dés...")
+    
+    # 
+
+if __name__ == "__main__":
+    main()
+
